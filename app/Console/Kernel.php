add4782e54e433acf8686280ee9270df2398bac3
Shivam <shivam.singla159@gmail.com>
2024-10-24 01:34:43 +0530

    added command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +14,12 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('app:meal-planning')->everyFiveMinutes()->before(function () {
+            Log::info('Meal monthly planning command is about to run');
+        });
+        // $schedule->call(function () {
+        //     Log::info('Scheduled task is running');
+        // })->everyFiveMinutes();
     }
 
     /**
